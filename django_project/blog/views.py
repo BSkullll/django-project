48350690931fec3a2b65d273f57710df0cd45109
BSkullll <gaurav.gk3676@gmail.com>
2019-11-27 08:06:47 -0800

    Adding MOdels and remove dummy data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Post
 # Create your views here.
 
-posts = [
-	{
-		'author': 'Gaurav Kumar',
-		'title': 'Blog Post 1',
-		'content': 'First post contetnt',
-		'date_posted': 'August 28 2019'
-	},
-	{
-		'author': 'Saurav Kumar',
-		'title': 'Blog Post 2 ',
-		'content': 'Second post content',
-		'date_posted': 'August 29 2019'
-	}	
-]
 
 
 def home(request):
 	data = {
-		'posts':posts,
+		'posts':Post.objects.all(),
 		'title': "Home",
 	}
 	return render(request, 'blog/home.html', context=data)
