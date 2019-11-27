d36b3423db88ee1d9b2e5b7fc17981bb7b88e535
BSkullll <gaurav.gk3676@gmail.com>
2019-11-27 05:47:45 -0800

    Adding Css and makes Pretty

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,8 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+	{
+		'author': 'Gaurav Kumar',
+		'title': 'Blog Post 1',
+		'content': 'First post contetnt',
+		'date_posted': 'August 28 2019'
+	},
+	{
+		'author': 'Saurav Kumar',
+		'title': 'Blog Post 2 ',
+		'content': 'Second post content',
+		'date_posted': 'August 29 2019'
+	}	
+]
+
+
 def home(request):
-	return HttpResponse('<h1> Blog Home </h1>')
+	data = {
+		'posts':posts,
+		'title': "Home",
+	}
+	return render(request, 'blog/home.html', context=data)
 
 def about(request):
-	return HttpResponse('<h1> Blog About </h1>')	
+	data = {
+		'title': 'About',
+	}
+	return render(request, 'blog/about.html', context=data)	
