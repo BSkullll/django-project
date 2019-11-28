4f50020c01a4261872db4345c27b91d9cdcf8c63
BSkullll <gaurav.gk3676@gmail.com>
2019-11-27 22:21:20 -0800

    making forms with crispy_form

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from users import views as user_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('blog.urls'))
+    url(r'^', include('blog.urls')),
+    url(r'^register/$', user_views.register, name='register'),
 ]
