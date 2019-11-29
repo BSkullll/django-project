2c712b77037c398d2705fb0a2c2a09e108d9632c
BSkullll <gaurav.gk3676@gmail.com>
2019-11-29 08:37:28 -0800

    Updated

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from users.models import Profile
 
 
 class UserRegisterForm(UserCreationForm, forms.Form):
@@ -17,3 +18,35 @@ class UserRegisterForm(UserCreationForm, forms.Form):
 		if email and User.objects.filter(email=email).exclude(username=username).exists():
 			raise forms.ValidationError(u'This email already exists.')
 		return email	
+
+class UserUpdateForm(forms.ModelForm):
+	email = forms.EmailField()
+	class Meta:
+		model = User
+		fields = ['username','email',]
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		username = self.cleaned_data.get('username')
+
+		if email and User.objects.filter(email=email).exclude(username=username).exists():
+			if len(User.objects.filter(email=email).exclude(username=username))>1:
+				raise forms.ValidationError(u'This email already exists.')
+			return email
+		return email	
+	
+	def clean_username(self):
+		
+		username = self.cleaned_data.get('username')
+
+		if username and User.objects.filter(username=username).exists():
+			if len(User.objects.filter(username=username))>1:
+				raise forms.ValidationError(u'This username already exists.')
+			else:
+				return username	
+		return username		
+
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['image']		
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from users.forms import UserRegisterForm 
+from users.forms import UserUpdateForm , ProfileUpdateForm
+
 # Create your views here.
 
 def register(request):
 	if request.method == "POST":
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
+			
+			
 			form.save()
 			username = form.cleaned_data.get('username')
 			messages.success(request, f'Your account has been created! You are now able to log in')
@@ -20,4 +24,26 @@ def register(request):
 
 @login_required
 def profile(request):
-	return render(request, 'users/profile.html')	
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance=request.user)
+		p_form = ProfileUpdateForm(
+							request.POST,
+							request.FILES,
+					  		instance=request.user.profile)
+
+		if u_form.is_valid() and p_form.is_valid():
+			
+			
+			u_form.save()
+			p_form.save()
+			messages.success(request, f'Your account has been updated!')
+			return redirect('blog-home')
+	else:
+		u_form = UserUpdateForm(instance=request.user)
+		p_form = ProfileUpdateForm(instance=request.user.profile)	
+
+	context = {
+		'u_form': u_form,
+		'p_form': p_form,
+	}
+	return render(request, 'users/profile.html', context=context)	
