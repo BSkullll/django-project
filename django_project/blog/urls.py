3bf47767eb72de21636cf70f2dde0f5c9197d87a
BSkullll <gaurav.gk3676@gmail.com>
2019-12-01 01:32:35 -0800

    Done With Pagination

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -3,10 +3,13 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.PostListView.as_view(), name='blog-home'),
+	url(r'^user/(?P<username>[\w.@+-]+)/$', views.UserPostListView.as_view(), name='postss-users'),
 	url(r'^post/(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post-detail'),
 	url(r'^post/new/$', views.PostCreateView.as_view(), name='post-create'),
 	url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post-update'),
 	url(r'^post/(?P<pk>\d+)/delete/$', views.PostDeleteView.as_view(), name='post-delete'),
-	url(r'^about/$', views.about, name='blog-about')
+	url(r'^about/$', views.about, name='blog-about'),
+
+
 
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
@@ -26,7 +27,20 @@ class PostListView(ListView):
 	model = Post
 	template_name = 'blog/home.html'
 	context_object_name = 'posts'
-	ordering = ['-date_posted'] 
+	ordering = ['-date_posted']
+	paginate_by = 6 
+
+class UserPostListView(ListView):
+	model = Post
+	template_name = 'blog/user_posts_list.html'
+	context_object_name = 'posts'
+	# ordering = ['-date_posted']
+	paginate_by = 6 
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
 	model = Post
